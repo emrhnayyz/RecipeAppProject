6820c199160d851c82f6c5edd41255f156eb811b
emrhnayz <emirhan.ayaz171@gmail.com>
2023-06-03 04:09:17 +0200

    first commit

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import "./NavbarStyles";
+import { Hamburger, Logo, Menu, MenuLink, Nav } from './NavbarStyles';
+import { GiHamburgerMenu } from "react-icons/gi"; 
+
+const Navbar = () => {
+
+const [acik, setAcik] = useState(false)
+
+
+  return (
+    <Nav>
+<Logo to="/home" onClick={()=> setAcik(false)}>
+
+<i>{"<Clarusway/>"} </i>
+{/* tag lere özel < işaretlerini string gibi kullanmak istersek üstteki syntax la yazarız */}
+
+<span>recipe</span>
+
+
+</Logo>
+
+<Hamburger onClick={()=> setAcik(!acik)}>
+<GiHamburgerMenu/>
+
+</Hamburger>
+
+
+
+<Menu  osman = {acik} onClick={()=> setAcik(false)}>
+<MenuLink to="/about">
+About
+</MenuLink>
+
+
+
+<div>
+  <a target="_blank" href="https://github.com/emrhnayyz">Github</a>
+</div>
+
+<MenuLink to="/">
+Logout
+</MenuLink>
+
+
+</Menu>
+
+    </Nav>
+  )
+}
+
+export default Navbar
